821fbeea4a61cbe93e152b2d518e8f682446fab5
Tomás Antunes <tomasbantunes@gmail.com>
2021-01-09 20:39:50 +0000

    Correção de alguns erros, adição do ficheiro board_file.txt para guardar o tabuleiro

--- 48511_48951/src/mine_functions.c
+++ 48511_48951/src/mine_functions.c
@@ -1,13 +1,7 @@
-// APAGAR ANTES DE ENTREGAR
-// https://stackoverflow.com/questions/50320998/minesweeper-in-c/50321042 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-int x, y;
-float diff;
-int mines= 0;
 int blankBoard[26][26];
 int finalBoard[26][26];
 
@@ -17,16 +11,20 @@ void generateBoard(int board[26][26], int nLines, int nCols, int nMines){
     // preenche um tabuleiro (par√¢metro board) de dimens√£o
     // nLin linhas e nCol colunas com nMines minas. Utilize
     // a fun√ß√£o rand() (stdlib.h) para gerar a posi√ß√£o de cada mina
-	
-	for(int i = 0; i < nLines; i++){ // cada "espaÁo" no tabuleiro quando ainda tapado È preenchido com um -
-		for(int j = 0; j < nCols; j++){
+    
+
+	int i, j, mines=0;
+
+	for(i = 0; i < nLines; i++){ // cada "espaÁo" no tabuleiro quando ainda tapado È preenchido com um -
+		for(j = 0; j < nCols; j++){
 			board[i][j] = '-';
 			blankBoard[i][j] = board[i][j];
 			finalBoard[i][j] = board[i][j];
 		}
 	}
 
-	i, j = 0;
+	i = 0;
+	j = 0;
 
 	while(mines < nMines){ // definiÁ„o da posiÁ„o das minas
 		i = rand()%nLines;
@@ -39,7 +37,8 @@ void generateBoard(int board[26][26], int nLines, int nCols, int nMines){
 
 	}
 
-	i, j = 0;
+	i = 0;
+	j = 0; 
 
 	while(i < nLines){ // gerar o valo de cada espaÁinho tendo em conta a posiÁ„o das minas
 		while(j<nCols){
@@ -83,8 +82,17 @@ void generateBoard(int board[26][26], int nLines, int nCols, int nMines){
 		j = 0;
 		i++;
 	}
-	i,j = 0;
-	    
+	i = 0;
+	j = 0; 
+
+	FILE *f = fopen("board_file.txt", "wb");
+	for(i = 0; i<nLines; i++){
+		for(j=0; j<nCols; j++){
+			fputc(finalBoard[i][j], f);
+		}
+	}
+	fclose(f);
+
 }
 
 void printBoard(int board[26][26], int nLines, int nCols, bool showMines){
@@ -107,12 +115,12 @@ void printBoard(int board[26][26], int nLines, int nCols, bool showMines){
 		printf("|%d|\t", j);
 		while(i<nLines){ 
 			if(blankBoard[i][j] == '-'){
-				print("|%c|\t", blankBoard[i][j]);
+				printf("|%c|\t", blankBoard[i][j]);
 			} else if(board[i][j] == 0){
 				blankBoard[i][j] = 'B'; // altera qualquer valor 0 para a letra B (blank)
-				print("|%c|\t", blankBoard[i][j]);
+				printf("|%c|\t", blankBoard[i][j]);
 			} else {
-				print("|%d|\t", blankBoard[i][j]);
+				printf("|%d|\t", blankBoard[i][j]);
 			} 
 			i++;
 		}
@@ -120,7 +128,16 @@ void printBoard(int board[26][26], int nLines, int nCols, bool showMines){
 		i = 0;
 		j++;
 	}
-    
+
+/**	for(int k=0; k<nLines; k++){
+		for(int l=0; l<nCols; l++){
+			printf("|%c|\t", board[k][l]);
+		}
+		printf("\n");
+	}
+
+	printf("\n\n");
+**/    
 }
 
 int uncover(int board[26][26], int nLines, int nCols, int line, int col){
--- 48511_48951/src/minesweeper.c
+++ 48511_48951/src/minesweeper.c
@@ -1,44 +1,60 @@
-#include <stdio.h>
 #include "mine_functions.c"
 
 int main(){
-    int lines, cols, mines, dificuldade, choice;
+    	int lines, cols, wMines, dificuldade, choice, show;
 
-    int board[26][26];
+    	int board[26][26];
+
+	bool wShow = false;
+
+	char sdiff[200] = "Escolha dificuldade (1-fácil - 7 minas (10x10), 2-médio - 10 minas (8x8), 3-difícil - 10 minas (5x5)): ";
     
-    printf("0 - Exit, 1 - Generate Board, 2 - Read Board: ");
-    scanf("%d", &choice);
-
-    if(choice == 0)
-    {
-        exit(0);
-    }
-    else if(choice == 1)
-    {
-        printf("Escolha dificuldade (1-fácil - 7 minas (10x10), 2-médio - 10 minas (8x8), 3-difícil - 10 minas (5x5)): ");
+    
+    	printf("%s", sdiff);
         scanf("%d", &dificuldade); 
 
         if (dificuldade == 1){
 
-            mines = 7;
-            lines, cols = 10;
+            	wMines = 7;
+            	lines = 10; 
+		cols = 10;
 
         } else if (dificuldade == 2){
         
-            mines = 10;
-            lines, cols = 8;
+            	wMines = 10;
+            	lines = 8;
+		cols = 8;
 
         } else if (dificuldade == 3)
         {
-            mines = 10;
-            lines, cols = 5;
+            	wMines = 10;
+            	lines = 5;
+		cols = 5;
         }
-    }
-    else if (choice == 2)
-    {
-        
-    }
+	
+	while(choice != 0){
+		char ch[200] = "0 - Exit, 1 - Generate Board, 2 - Read Board: ";
+		printf("%s", ch);
+		scanf("%d", &choice);
+ 
+		if(choice == 0){
+	
+        		exit(0);
 
+	    	}else if(choice == 1){
 
+			generateBoard(board, lines, cols, wMines);
 
+		}else if(choice == 2){
+			printf("Quer mostrar as minas?(sim = 1, nao = 0) ");
+			scanf("%d", &show);
+			if(show == 1){
+				wShow = true;
+				printBoard(board, lines, cols, wShow);
+			}else if(show == 0){
+				wShow = false;
+				printBoard(board, lines, cols, wShow);
+			}
+		}
+	}
 }
